b80cf3a1a59defeaaaa07706cd6c70d63f0b43aa
Alexia Ariana <alarabregode@ittepic.edu.mx>
2018-05-29 18:07:34 -0600

    Creacion servicio

--- src/services/proveedor.ts
+++ src/services/proveedor.ts
@@ -4,10 +4,26 @@ import {Http} from "@angular/http";
 @Injectable()
 export class ProveedorService{
     
-   
+   public data:any;
 
     constructor(private http:Http){
        
+    }
+
+    getQuotes(){
+       // this.http.get("http://localhost:8080/users.json")
+       this.http.get("https://api.myjson.com/bins/ka7yq")
+        .subscribe(
+            res=>{
+                this.data = res.json();
+                console.log(this.data);
+            },
+            error=>{
+                console.log(error);
+
+            }
+            );
+            
     }
 
    
